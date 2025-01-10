708d2e77d97dbb8060a164ba51d33edee2535d06
Andria11rt <andiaandria21@gmail.com>
2025-01-10 19:57:18 +0400

    second commit

---
+++ level70/classwork/script.js
@@ -0,0 +1,75 @@
+
+// console.log("Hello " +  5  +  "year") შეიზლება string -ს მიუმატო integer -ი
+
+// -------------------------------------
+
+// variebles / ცვლადები 
+
+// let
+
+// let name = "Andria";
+// name = "angelina";
+// console.log(name);
+
+// const შეცვლა არ შეიძლება მაინც პირველი დარჩება
+
+// const name = "Andria";
+
+// name = "Dato"; 
+
+// console.log(name);
+
+
+// var არ ვიყენებთ
+
+// var name = "Andria"; 
+// var lastname = "Nachkebia";
+// console.log(name, lastname);
+
+// var name = "koko";
+// var lastname = "kuku";
+
+
+
+
+
+
+
+// let sayHelloToUser = "Hello User";
+
+// Camel case დიდი ასოები  sayHelloToYou
+
+// console.log(sayHelloToUser);
+
+
+
+
+
+
+
+
+// let name = prompt("Enter your name:"); promt - არის იგვე input
+// console.log("User name is " + name);
+// alert("User name is " + name)
+
+
+
+
+// let phoneNumber = promt("Enter your number:");
+
+// const userNumber = phoneNumber;
+
+// console.log("User number is " + userNumber);
+
+
+
+let num = Number(prompt("Ent Num: "));
+
+console.log(typeof num);
+
+
+//  JS         PY
+// Number() - int()
+// String() - str()
+// parsefloat() - float()
+// typeof() ---- type()
---
+++ level70/classwork/script1.js
@@ -0,0 +1,16 @@
+
+let name = "ანდრია";
+
+let lastname = "ნაჭყებია";
+
+let year = 13;
+
+let favcar = "ბუგატი"
+
+let favcolor = "წითელი"
+
+let all = name + " " + lastname + " " + year + " " + favcar + " " + favcolor
+
+alert(all)
+
+console.log(all);
