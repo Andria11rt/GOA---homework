e837db761d7cef9867d79dcc3647c895c6ba9fbd
Andria11rt <andiaandria21@gmail.com>
2025-01-19 20:27:04 +0400

    second commit

---
+++ level73/classwork/script.js
@@ -0,0 +1,20 @@
+
+/*function grade(s1, s2, s3) {
+    let avg = (s1 + s2 + s3) / 3;
+    if(90 <= avg <=100){
+        alert("A")
+    } 
+    elseif(80 <= avg < 90){
+        alert("B")
+    } 
+    elseif(70 <= avg < 80){
+        alert("C")
+    } 
+    elseif(60 <= avg < 70){
+        alert("D")
+    } 
+    else { 
+        alert("F")
+    }
+
+}*/
