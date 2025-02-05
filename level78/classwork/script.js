6ec75e7c113bfef8dc864604f95af638171ede21
Andria11rt <andiaandria21@gmail.com>
2025-02-05 16:53:19 +0400

    second commit

---
+++ level78/classwork/script.js
@@ -0,0 +1,49 @@
+//Conditional statments
+
+/* let age = 19
+
+if(age >= 18) {
+    console.log("შენ შეგიძლია");
+}else{
+    console.log("შენ არ შეგიძლია");
+} */
+
+
+//-------------------------------
+
+
+// Ternary operator
+
+// let age = 19
+//           True   True = მაშინ გამოვა ეს : False = მაშინ გამოვა ეს ტექსტი
+// let res = (age >= 18) ? "შენ შეგიძლია" : "შენ არ შეგიძლია"
+
+// console.log(res);
+
+
+// console.log((age >= 18) ? "შენ შეგიძლია" : "შენ არ შეგიძლია");
+
+
+/*let age = Number(prompt("Enter your age:"))
+    not correct
+let res = (age <= 12) ? "ჯერ ბავშვი ხარ" :
+          (age <= 17) ? "თინეიჯერი ხარ" :
+          (age == 18) ? "სრულწლოვანი ხარ" :
+          (age >= 50 && age <= 51) ? "ნახევარი საუკუნე გაიარე" :
+          (age >= 60 && age <= 100) ? "დაბერდი პაპი" : ' ტკბილად განისვენე'
+
+console.log(res);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(true || false);
