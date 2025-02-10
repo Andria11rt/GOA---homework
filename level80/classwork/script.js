a2340e92d01dec4857d9cd23fe165ca9c8de9516
Andria11rt <andiaandria21@gmail.com>
2025-02-10 19:30:02 +0400

    second commit

---
+++ level80/classwork/script.js
@@ -0,0 +1,44 @@
+// let i = 10
+
+// while(i > 0) {
+
+//    console.log("Hello World!");
+
+//    i--;
+// }
+
+
+
+//          True
+// while("hello" == "hello") {
+//    console.log("Hello World");
+    
+// }
+
+
+
+
+
+for(let i = 0; i < 10; i++) {
+    console.log("Hello World!");
+}
+
+
+
+
+let i = 10
+
+while(i > 0) {
+   console.log("Hello World!");
+
+   i--;
+}
+
+
+
+// განსხვავებები / Differences
+
+// 1. სინტაქსი / syntax
+// 2. დანიშნულება / 
+// 3. მუშაობა / work
+// 4. მოქნილობა / flexibility
---
+++ level80/classwork/script1.js
@@ -0,0 +1,36 @@
+/*let i = 50
+
+while(i > 0) {
+   console.log("Goa");
+
+   i--;
+} */
+
+
+/*let i = 20
+
+while(i >= 0) {
+   console.log(Number(i));
+
+   i--;
+} */
+
+
+/*let i = 2
+
+while(i <= 20){
+   console.log(Number(i));
+   i++;
+   i++;
+}*/
+
+let i = 5
+
+while(i <= 100){
+   console.log(Number(i));
+   i++;
+   i++;
+   i++;
+   i++;
+   i++;
+}
