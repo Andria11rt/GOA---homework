649d46dcee14339d573ed711b31ecb77ac935186
Andria11rt <andiaandria21@gmail.com>
2025-03-21 21:18:48 +0400

    second commit

---
+++ level90/classwork/script.js
@@ -0,0 +1,36 @@
+/*
+function Contact(name, number){
+    this.name = name;
+    this.number = number;
+    this.print = function(){
+        return`${name}: ${number}`
+    }
+}
+
+let user1 = new Contact("Andria", 56382638)
+console.log(user1.print())
+*/
+
+
+
+//              0        1        2     3    4     5      6                    7                                          8
+// let names = ["Davit", "Nodar", "Goga", 100, true, false, 10.22,{name: "Andria", lastName: "Nachkebia"}, ['football', "basketball", "golf"]];
+
+
+// console.log(names[0] + "  " + names[1])
+
+// console.log(names[7].name);
+// console.log(names[8][1])
+
+
+
+
+// let courses = new Array("HTML", "CSS", "JS")
+
+// courses[1] = "tailwildcss"
+// console.log(courses[5])
+
+let name = "Andria"
+let lastname = "Nchkebia"
+let courses = new Array(name, lastname)
+console.log(courses)
---
+++ level90/homework/script.js
@@ -0,0 +1,17 @@
+
+let mix = ["Andria", true, false, 10.22, {lastName: "Nachkebia"}, ['football']]
+
+console.log(mix[1]);
+console.log(mix[2]);
+console.log(mix[3]);
+console.log(mix[4]);
+console.log(mix[5]);
+
+
+mix[1] = "Luka"
+mix[2] = false
+mix[3] = true
+mix[4] = 9.99999
+mix[5] = "Davitaze"
+
+console.log(mix)
