a75095a4d42534b29318c6924ce41765cbe083a3
Andria11rt <andiaandria21@gmail.com>
2025-05-04 17:29:36 +0400

    second commit

---
+++ level103/classwork/script1.js
@@ -0,0 +1,26 @@
+
+let htmlBtn = document.querySelector('.htmlBtn');
+let cssBtn = document.querySelector('.cssBtn');
+let jsBtn = document.querySelector('.jsBtn');
+
+let img = document.querySelector();
+
+console.log(htmlBtn, cssBtn, jsBtn, img);
+
+function html() {
+    img.src = "https://www.devopsschool.com/blog/wp-content/uploads/2023/07/image-249.png";
+};
+  
+  function css() {
+    img.src = "https://blog.mystrika.com/wp-content/uploads/sites/8/2023/10/HTML-CSS.jpeg";
+};
+  
+  function js() {
+    img.src = "https://w3camps.com/wp-content/uploads/2023/01/what-is-js-cover.jpg";
+};
+
+htmlBtnbtn.addEventListener("click", html)
+
+cssBtn.addEventListener("click", css)
+
+jsBtn.addEventListener("click", js)
